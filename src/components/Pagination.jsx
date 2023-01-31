4cc4bb04a3205e5f6c55091298f3df23c92d20d3
Gustavo Verdugo <gverdugo@sb.cl>
2023-01-31 14:23:02 -0300

    update

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,18 +4,17 @@ import { DataContext } from '../context/DataContext';
 import { PROD } from "../config";
 const Pagination = () => {
     const { loading, savePagination, pagination, query, page, setPage, saveProduct, setSearching } = useContext(DataContext);
-    async function useCallProductsPagination(p) {
+    async function useCallProductsPagination() {
         setSearching(true)
-        await setPage(p)
-        if (p > 0 && p < pagination) {
+        if (page > 0 && page < pagination) {
             if (query != null) {
-                const response = await fetch(`${PROD}productos?${query}&populate=*&pagination[page]=${p}&pagination[pageSize]=15`)
+                const response = await fetch(`${PROD}productos?${query}&populate=*&pagination[page]=${page}&pagination[pageSize]=15`)
                 const data = await response.json()
                 await saveProduct(data.data);
                 await savePagination(data.meta.pagination.pageCount)
                 setSearching(false);
             } else {
-                const response = await fetch(`${PROD}productos?populate=*&pagination[page]=${p}&pagination[pageSize]=15`)
+                const response = await fetch(`${PROD}productos?populate=*&pagination[page]=${page}&pagination[pageSize]=15`)
                 const data = await response.json()
                 await saveProduct(data.data);
                 await savePagination(data.meta.pagination.pageCount)
@@ -24,6 +23,9 @@ const Pagination = () => {
 
         }
     }
+    useEffect(() => {
+        useCallProductsPagination();
+    }, [page])
     return (
         <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
             {
@@ -34,14 +36,14 @@ const Pagination = () => {
                                 <button
                                     disabled={page === 1 ? true : false}
                                     className="relative inline-flex items-center px-4 py-2 border border-gray-200 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 disabled:text-gray bg-gray"
-                                    onClick={useCallProductsPagination(page - 1)}
+                                    onClick={() => { setPage(page - 1) }}
                                 >
                                     Anterior
                                 </button>
                                 <button
                                     disabled={page === pagination ? true : false}
                                     className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-200 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-                                    onClick={useCallProductsPagination(page + 1)}
+                                    onClick={() => { setPage(page + 1) }}
                                 >
                                     Siguiente
                                 </button>
@@ -51,7 +53,7 @@ const Pagination = () => {
                                     <nav className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
                                         <button
                                             disabled={page === 1 ? true : false}
-                                            onClick={useCallProductsPagination(page - 1)}
+                                            onClick={() => { setPage(page - 1) }}
                                             className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 disabled:text-gray bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
                                         >
                                             <span className="sr-only">Anterior</span>
@@ -62,7 +64,7 @@ const Pagination = () => {
                                         <button
                                             disabled={page === pagination ? true : false}
                                             className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 disabled:text-gray bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
-                                            onClick={useCallProductsPagination(page + 1)}
+                                            onClick={() => { setPage(page + 1) }}
                                         >
                                             <span className="sr-only">Siguiente</span>
                                             <span>Siguiente</span>
