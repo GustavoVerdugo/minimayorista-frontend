03ff357a720b205f558d61715d96b5623afff7d7
Gustavo Verdugo <gverdugo@sb.cl>
2023-02-01 10:09:59 -0300

    update

--- pages/api/result.js
+++ pages/api/result.js
@@ -1,3 +1,16 @@
-export default function handler(req, res) {
-    res.redirect(307, "https://minimayorista-web.onrender.com/result");
+export default async function handler(req, res) {
+    let token = localStorage.getItem('tkn')
+    const response = await fetch(`https://payments-nple.onrender.com/apiFlow/result`, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify(dt) // body data type must match "Content-Type" header
+    });
+    const data = await response.json();
+    console.log(data);
+    res.redirect(307, "/result");
+    return data;
 }
