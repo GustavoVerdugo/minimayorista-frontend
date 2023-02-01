d1bb3509c9d8f8bd1dd00f78aa212d15ca3084b7
Gustavo Verdugo <gverdugo@sb.cl>
2023-02-01 08:49:02 -0300

    update

--- src/components/ResultPayments.jsx
+++ src/components/ResultPayments.jsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import React, { useEffect } from 'react'
-import { confirmPay } from '../services/PayService'
 
 export default function ResultPayments() {
   return (
