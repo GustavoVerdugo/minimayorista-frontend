85b11855b122d160bdb4ff011eb2eb7556c36657
Gustavo Verdugo <gverdugo@sb.cl>
2023-02-21 13:51:09 -0300

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -6,9 +6,26 @@ import Script from 'next/script'
 function MyApp({ Component, pageProps }) {
 
   return (
+    <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-3Y832DGM3" />
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-3Y832DGM3R', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
       <DataProvider>
         <Component {...pageProps} />
       </DataProvider>
+    </>
   )
 }
 
