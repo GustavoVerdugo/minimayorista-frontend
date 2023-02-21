a144fb273ad683314cd0cccc62916a07081717fd
Gustavo Verdugo <gverdugo@sb.cl>
2023-02-21 13:31:44 -0300

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,31 @@
 import { DataProvider } from '../src/context/DataContext';
 import useFetch from '../src/helpers/useFetch';
 import '../styles/globals.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
 
   return (
-    <DataProvider>
-      <Component {...pageProps} />
-    </DataProvider>
+    <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-3Y832DGM3" />
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-3Y832DGM3R', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+      <DataProvider>
+        <Component {...pageProps} />
+      </DataProvider>
+    </>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,6 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet" />
-
       </Head>
 
 
